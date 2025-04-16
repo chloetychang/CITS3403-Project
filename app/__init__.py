cec37d4bdc965f7129e6eb3e2defc3d9bdc5518e
Chloe Chang <chloetychang@gmail.com>
2025-04-17 02:26:31 +0800

    Restructuring repository, fix routing, gitignore additions

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates/html', static_folder='app/static')
+
+if __name__ == "__main__":
+    app.run()
+    
 from app import routes
-
-
--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,16 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
+
+@app.route('/home')
+def home():
+    return "Hello, World!"
+
+@app.route('/visualisation')
+def visualisation():
+    return "Hello, Visualisation!"
+
+@app.route("/share")
+def share():
+    return "Hello, Share!"
