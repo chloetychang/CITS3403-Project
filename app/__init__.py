46cdcd5d2e2bfb0528b00640fcfd954f6c3c76ae
Chloe Chang <chloetychang@gmail.com>
2025-04-30 17:37:06 +0800

    Linking to a database with SQLAlchemy

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,16 @@
 # Handles app creation and configuration
 
 from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+from app.config import Config
+from app import models
 
 app = Flask(__name__, static_folder='static')
+app.config.from_object(Config)                  # Configure the `app` object using our `Config` class
+db = SQLAlchemy(app)                            # Create SQLAlchemy object called db
+migrate = Migrate(app, db)                      # Create a migrate object
+
 app.secret_key = 'monke-magic'                  # reminder to not include secret key in final version.
     
 if __name__ == "__main__":
