76999acb77d68768ab0a18594882490657b76009
Chloe Chang <chloetychang@gmail.com>
2025-05-01 02:29:35 +0800

    add password_hash field, finalise and simplify schema, add comments

--- app/models.py
+++ app/models.py
@@ -3,6 +3,7 @@ from app import db
 class User(db.Model):
     user_id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
+    password_hash = db.Column(db.String(120), nullable=False)    # hashed password
     # can potentially add more fields - such as age, public/private profile, etc.
 
 class Entry(db.Model):
@@ -11,5 +12,5 @@ class Entry(db.Model):
     sleep_datetime = db.Column(db.DateTime, nullable=False)
     wake_datetime = db.Column(db.DateTime, nullable=True)
     mood = db.Column(db.Integer, nullable=True)       # mood - indexed for future calculations
-    created_at = db.Column(db.DateTime, nullable=True)
-    last_modified = db.Column(db.DateTime, nullable=True)
+    # sleep duration - calculated from sleep and wake datetime (field not required here)
+    # sleep quality - calculated from mood and sleep duration (field not required here)
