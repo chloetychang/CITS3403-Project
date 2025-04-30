dcf7d9df10a031fef61cd3d081490ac41a29346b
Chloe Chang <chloetychang@gmail.com>
2025-05-01 03:02:34 +0800

    comment added - don't include secret key in final product

--- app/config.py
+++ app/config.py
@@ -5,4 +5,4 @@ default_database_location = 'sqlite:///' + os.path.join(basedir, 'app.db')
 
 class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL') or default_database_location
-    SECRET_KEY = "penguin-magic"
+    SECRET_KEY = "penguin-magic"    # reminder to not include secret key in final version.
