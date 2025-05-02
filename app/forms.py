524f122ba35662302330e05bd873911b143db784
KIMBERLEY <kimberley.lee08@gmail.com>
2025-05-02 12:33:23 +0800

    refactored login and signup using wtforms along with proper routing

--- app/forms.py
+++ app/forms.py
@@ -1,10 +1,11 @@
 # Define forms using Flask-WTF
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SelectField, IntegerField, SubmitField
-from wtforms.validators import DataRequired, Email, Length
+from wtforms.validators import DataRequired, Email, Length, EqualTo
+
 
 class LoginForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(), Email()])
+    email = StringField('Email', validators=[DataRequired(), Email(message='Please enter a valid email address')])
     password = PasswordField('Password', validators=[DataRequired(), Length(min=6)])
     submit = SubmitField('Login')
     
@@ -13,9 +14,11 @@ class SignupForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(max=50)])
     age = IntegerField('Age', validators=[DataRequired()])
     gender = SelectField('Gender', choices=[('male', 'Male'), ('female', 'Female'), ('prefer_not_to_say', 'Prefer not to say')])
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired(), Length(min=6)])    password = PasswordField('Password', validators=[
+    email = StringField('Email', validators=[DataRequired(), Email(message='Please enter a valid email address')])
+    password = PasswordField('Password', validators=[
         DataRequired(),
-        Length(min=6)
-    ]) 
+        Length(min=6),
+        EqualTo('confirm_password', message='Passwords must match')
+    ])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired()])
     submit = SubmitField('Sign Up')
