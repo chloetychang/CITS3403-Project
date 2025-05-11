6f7b15778904e65b4c5d6441a7639736537b1acc
KIMBERLEY <kimberley.lee08@gmail.com>
2025-05-11 17:29:40 +0800

    delete fallback value

--- app/config.py
+++ app/config.py
@@ -5,4 +5,4 @@ default_database_location = 'sqlite:///' + os.path.join(basedir, 'app.db')
 
 class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL') or default_database_location
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'default-secret-key')  # Use env variable
+    SECRET_KEY = os.environ.get('SECRET_KEY') # Configures the secret key for the app
