2dc52ddefb86b63e3aaf06303358595964ec7f2e
Chloe Chang <chloetychang@gmail.com>
2025-05-13 15:36:18 +0800

    Creating multiple configuration options

--- app/config.py
+++ app/config.py
@@ -4,5 +4,13 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 default_database_location = 'sqlite:///' + os.path.join(basedir, 'app.db')
 
 class Config:
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL') or default_database_location
     SECRET_KEY = os.environ.get('SECRET_KEY') # Configures the secret key for the app
+    
+class DeploymentConfig(Config):
+    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "test.db")
+
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = "sqlite:///:memory"
+    TESTING = True
