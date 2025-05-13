a44b69f91438282aaa7f1f9ab9f5de3a5a7b3705
Chloe Chang <chloetychang@gmail.com>
2025-05-13 18:12:45 +0800

    Modify all endpoints and config change (avoids hardcoding db)

--- app/config.py
+++ app/config.py
@@ -5,11 +5,10 @@ default_database_location = 'sqlite:///' + os.path.join(basedir, 'app.db')
 
 class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL') or default_database_location
     SECRET_KEY = os.environ.get('SECRET_KEY') # Configures the secret key for the app
     
 class DeploymentConfig(Config):
-    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "test.db")
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL') or default_database_location
 
 class TestConfig(Config):
     SQLALCHEMY_DATABASE_URI = "sqlite:///:memory"
