e5c3e177d701b09aae80fa5eba6e2e4c91c56129
KIMBERLEY <kimberley.lee08@gmail.com>
2025-05-14 22:58:51 +0800

    comments added

--- app/systemTest.py
+++ app/systemTest.py
@@ -1,8 +1,11 @@
 import multiprocessing
 import unittest
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
+
+# Uncomment the following line if you need to use ChromeDriverManager
+# from selenium.webdriver.chrome.service import Service
+# from selenium.webdriver.chrome.options import Options
+# from webdriver_manager.chrome import ChromeDriverManager
 from app.config import TestConfig
 from app import create_app, db
 from app.models import User, Entry  # add more models as needed
@@ -20,6 +23,8 @@ class SeleniumTests(unittest.TestCase):
         # Set up database
         db.create_all()
 
+        # Add Chrome configurations
+
         # Start the Flask server in a separate process
         self.server_thread = multiprocessing.Process(target=self.testApp.run)
         self.server_thread.start()
@@ -33,7 +38,7 @@ class SeleniumTests(unittest.TestCase):
         # Clean up resources
         self.server_thread.terminate()
         self.driver.close()
-        
+
         # Clear the database
         db.session.remove()
         db.drop_all()
