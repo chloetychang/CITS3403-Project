0a88cbb66defe192f8e0e4e7f274042b49b252ac
Chloe Chang <chloetychang@gmail.com>
2025-05-16 03:43:31 +0800

    Added another additional 5 test cases

--- tests/systemTest.py
+++ tests/systemTest.py
@@ -101,9 +101,57 @@ class SeleniumTests(unittest.TestCase):
         )
         self.assertIn("/signup", self.driver.current_url)
     
+    # Testing Login Functionality
+    ## This test will login and check if the user is redirected to the sleep page.
+    def test_login_functionality_success(self):
+        # Go to login page
+        self.driver.get(f"{self.base_url}/login")
+        
+        # Fill in the Form - Using the account "admin"
+        email_input = self.driver.find_element(By.ID, "email")
+        password_input = self.driver.find_element(By.ID, "password")
+        submit_button = self.driver.find_element(By.ID, "submit")
+
+        email_input.send_keys("pingu@test.com")
+        password_input.send_keys("test123")
+        submit_button.click()
+        
+        time.sleep(2)
+
+        # Check that user is now inside sleep page.
+        self.assertIn("/sleep", self.driver.current_url)
+        
+    ## This test will login with an incorrect password. The user should not be redirected to the sleep page.
+    def test_login_invalid_password(self):
+        self.driver.get(f"{self.base_url}/login")
+        
+        self.driver.find_element(By.ID, "email").send_keys("pingu@test.com")
+        self.driver.find_element(By.ID, "password").send_keys("wrongpass")          # wrong password
+        self.driver.find_element(By.ID, "submit").click()
+
+        time.sleep(1)
+        self.assertIn("/login", self.driver.current_url)
+        error_flash = WebDriverWait(self.driver, 5).until(
+            EC.presence_of_element_located((By.XPATH, "//*[contains(text(), 'Invalid email or password.')]"))
+        )
+        self.assertIn("Invalid email or password.", error_flash.text)
+
+    # This test will login with no credentials at all. The user should not be redirected to the sleep page.
+    def test_login_missing_fields(self):
+        self.driver.get(f"{self.base_url}/login")
+        
+        self.driver.find_element(By.ID, "email").send_keys("")  # leave blank
+        self.driver.find_element(By.ID, "password").send_keys("")  # leave blank
+        self.driver.find_element(By.ID, "submit").click()
+
+        time.sleep(1)
+        # Check that the URL hasn’t changed because form wasn’t submitted
+        self.assertIn("/login", self.driver.current_url)
+    
+    
     # Testing Signup Functionality
-    # This test will create a new user and check if the user is redirected to the login page
-    def test_signup_functionality(self):
+    ## This test will create a new user and check if the user is redirected to the login page
+    def test_signup_functionality_success(self):
         # Go to signup page
         self.driver.get(f"{self.base_url}/signup")
         # Fill in the Form
@@ -129,27 +177,91 @@ class SeleniumTests(unittest.TestCase):
         
         # Check that user is now redirected to login page.
         self.assertIn("/login", self.driver.current_url)
-    
-    # Testing Login Functionality
-    # This test will login and check if the user is redirected to the sleep page.
-    def test_login_functionality(self):
-        # Go to login page
-        self.driver.get(f"{self.base_url}/login")
         
-        # Fill in the Form - Using the account "admin"
+     ## This test will not create a new user because the username is already taken. The user should not be redirected to the login page.
+    def test_signup_functionality_duplicated_username(self):
+        # Go to signup page
+        self.driver.get(f"{self.base_url}/signup")
+        # Fill in the Form
+        name_input = self.driver.find_element(By.ID, "name")
+        username_input = self.driver.find_element(By.ID, "username")
+        age_input = self.driver.find_element(By.ID, "age")
+        select = Select(self.driver.find_element(By.ID, "gender"))
         email_input = self.driver.find_element(By.ID, "email")
         password_input = self.driver.find_element(By.ID, "password")
+        confirm_password_input = self.driver.find_element(By.ID, "confirm_password")
         submit_button = self.driver.find_element(By.ID, "submit")
-
-        email_input.send_keys("pingu@test.com")
+        
+        name_input.send_keys("Test User")
+        username_input.send_keys("pingu")
+        age_input.send_keys(25)
+        select.select_by_value("male")
+        email_input.send_keys("test@test.com")
         password_input.send_keys("test123")
+        confirm_password_input.send_keys("test123")
         submit_button.click()
         
         time.sleep(2)
-
-        # Check that user is now inside sleep page.
-        self.assertIn("/sleep", self.driver.current_url)
-    
+        
+        # Check that the URL hasn’t changed because form wasn’t submitted
+        self.assertIn("/signup", self.driver.current_url)
+        
+        error_flash = WebDriverWait(self.driver, 5).until(
+            EC.presence_of_element_located((By.XPATH, "//*[contains(text(), 'Username already taken')]"))
+        )
+        self.assertIn("Username already taken", error_flash.text)
+        
+    ## This test will not create a new user because the email is already taken. The user should not be redirected to the login page.
+    def test_signup_functionality_duplicated_email(self):
+        # Go to signup page
+        self.driver.get(f"{self.base_url}/signup")
+        # Fill in the Form
+        name_input = self.driver.find_element(By.ID, "name")
+        username_input = self.driver.find_element(By.ID, "username")
+        age_input = self.driver.find_element(By.ID, "age")
+        select = Select(self.driver.find_element(By.ID, "gender"))
+        email_input = self.driver.find_element(By.ID, "email")
+        password_input = self.driver.find_element(By.ID, "password")
+        confirm_password_input = self.driver.find_element(By.ID, "confirm_password")
+        submit_button = self.driver.find_element(By.ID, "submit")
+        
+        name_input.send_keys("Test User")
+        username_input.send_keys("testuser")
+        age_input.send_keys(25)
+        select.select_by_value("male")
+        email_input.send_keys("pingu@test.com")
+        password_input.send_keys("test123")
+        confirm_password_input.send_keys("test123")
+        submit_button.click()
+        
+        time.sleep(2)
+        
+        # Check that the URL hasn’t changed because form wasn’t submitted
+        self.assertIn("/signup", self.driver.current_url)
+        
+        error_flash = WebDriverWait(self.driver, 5).until(
+            EC.presence_of_element_located((By.XPATH, "//*[contains(text(), 'Email already registered')]"))
+        )
+        self.assertIn("Email already registered", error_flash.text)
+        
+    ## With no fields filled in, this test will not create a new user. The user should not be redirected to the login page.
+    def test_signup_functionality(self):
+        # Go to signup page
+        self.driver.get(f"{self.base_url}/signup")
+        
+        # Form not filled in
+        self.driver.find_element(By.ID, "name").send_keys("")
+        self.driver.find_element(By.ID, "username").send_keys("")
+        self.driver.find_element(By.ID, "age").send_keys("")
+        self.driver.find_element(By.ID, "email").send_keys("")
+        self.driver.find_element(By.ID, "password").send_keys("")
+        self.driver.find_element(By.ID, "confirm_password").send_keys("")
+        self.driver.find_element(By.ID, "submit").click()
+        
+        time.sleep(1)
+        # Check that the URL hasn’t changed because form wasn’t submitted
+        self.assertIn("/signup", self.driver.current_url)
+        
     # Testing upload form functionality
     # This test will check if the upload form will load and submit correctly, when all information is provided.
     def test_upload_form_functionality(self):
